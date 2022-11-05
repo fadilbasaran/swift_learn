de959f47d9a0603dbcc01c37518ef8a1d5a2c9c2
Fadıl Başaran <fadil.basaran.014@gmail.com>
2022-11-05 15:14:46 +0300

    Initial Commit

---
+++ Ios_Giris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ios_Giris
+//
+//  Created by Logical Defense Yazılım Arge Savunma San.ve Tic.Ltd.Şti. on 5.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
